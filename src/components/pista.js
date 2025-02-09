42eb9eed8f14abd1a384329412109b2dd41507b0
Mloaiza98 <mloaizaw@gmail.com>
2025-02-08 22:07:31 -0500

    Primera prueba de githubpages

--- src/components/pista.js
+++ src/components/pista.js
@@ -1,52 +1,48 @@
-import React, { useState } from 'react';
-import map1 from '../images/1.png';
-import map2 from '../images/2.png';
-import map3 from '../images/3.png';
-import map4 from '../images/4.png';
+import React, { useState } from "react";
+import map1 from "../images/1.png";
+import map2 from "../images/2.png";
+import map3 from "../images/3.png";
+import map4 from "../images/4.png";
 // import map5 from '../images/5.png';
-const mapas = [map1, map2, map3, map4 ]
-   
+const mapas = [map1, map2, map3, map4];
 
 console.log(mapas[0]);
-const Pista = ({ id, respuestaCorrecta, onRespuestaCorrecta, siguientePista }) => {
-  const [respuesta, setRespuesta] = useState('');
-  const [mensaje, setMensaje] = useState('');
-  const [Check, setCheck] = useState(null);
+const Pista = ({
+  id,
+  respuestaCorrecta,
+  onRespuestaCorrecta,
+  siguientePista,
+}) => {
+  const [respuesta, setRespuesta] = useState("");
   const handleInputChange = (e) => {
     setRespuesta(e.target.value);
   };
 
   const handleSubmit = (e) => {
-  
     e.preventDefault();
     if (respuesta.toLowerCase() === respuestaCorrecta.toLowerCase()) {
-      setMensaje('Esoooo chiquitica! ');
-      setCheck(true);
-      setRespuesta('');
+      setRespuesta("");
+      onRespuestaCorrecta();
+
       // setPistaProxima(siguientePista);
     } else {
-      setMensaje('Mehh, intenta de nuevo');
-      setRespuesta('');
+      setRespuesta("");
     }
-  };
-  const handleSubmitNext = (e) => {
-    onRespuestaCorrecta();
-      setMensaje('');
-      setCheck(null);
   };
 
   return (
     <div>
-      <img src={mapas[id-1]} alt="Mapa" height={"400px"}></img>
-      <form onSubmit={handleSubmit}>
-        <input type="numeric" value={respuesta} onChange={handleInputChange} onSubmit={handleSubmit}/>
-        {/* <button type="submit"></button> */}
-      </form>
-      {mensaje && <p>{mensaje}</p>}
-      {Check && 
-      <div> 
-      <button type="submit" onClick={handleSubmitNext}>¿Ya encontraste el regalo?</button>
-      </div>}
+      <img src={mapas[id - 1]} alt="Mapa" height={"400px"}></img>
+      <input
+        type="numeric"
+        value={respuesta}
+        onChange={handleInputChange}
+        placeholder="Introduce codigo"
+      />
+      <br />
+      <button type="submit" onClick={handleSubmit}>
+        ¿Ya encontraste el regalo?
+      </button>
     </div>
   );
 };
